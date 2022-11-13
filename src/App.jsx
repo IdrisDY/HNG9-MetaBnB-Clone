00b26797974bf62f93a35e055c19a3c6f6f7cda6
DisuAde <idrisadekunle726@gmail.com>
2022-11-13 21:04:07 +0100

    p correct,add route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import './App.css'
 import { Homepage } from './Components/Homepage'
 import { NftShow } from './Components/NftShow'
 import PurpleMockup from './Components/purpleMockup'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Homepage/>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Homepage/>}/>
+        <Route path='/nfts' element={ <NftShow/>}/>
+      </Routes>
+      </Router>
       {/* <PurpleMockup/> */}
     </div>
   )
--- src/Components/Reusable/Header.jsx
+++ src/Components/Reusable/Header.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import headerlogo from '../../assets/header.svg'
 import reacte from '../../assets/react.svg'
 import menu from '../../assets/outline_menu_black_24dp.png'
+import { Link } from 'react-router-dom'
 
 const MobileNav=({style,menuClick})=>(
   <div  className=' hidden flex flex-col text-white align-center justify-center pl-[15%] gap-5 md:pl-[10%] mobilenav' style={style}>
      <button onClick={menuClick} className='text-3xl text-black-900  mr-[50%] space-y-8'> X </button>
      <div className='  flex align-center justify-center'  >
 <ul className='nav-ul flex flex-col gap-6 lg:gap-16 md:gap-7 items-start w-full  '>
-   <li className='nav-list '>Home</li>
+   <li className='nav-list '><Link to='/' >Home</Link></li>
    <li className='nav-list'>Place to Stay</li>
-   <li className='nav-list'>NFTs</li>
+   <li className='nav-list'> <Link to='/nfts' >NFTs</Link></li>
    <li className='nav-list'>Community</li>
 </ul>
 </div>
@@ -36,9 +37,9 @@ const Header = () => {
       <div className=' hidden align-center justify-center lg:flex ' >
 
 <ul className='nav-ul flex lg:gap-16 md:gap-7 items-center w-full  '>
-   <li className='nav-list '>Home</li>
+   <li className='nav-list '><Link to='/' >Home</Link></li>
    <li className='nav-list'>Place to Stay</li>
-   <li className='nav-list'>NFTs</li>
+   <li className='nav-list'><Link to='/nfts' >NFTs</Link></li>
    <li className='nav-list'>Community</li>
 </ul>
 </div>
